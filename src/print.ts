50143a625be7e7cb4606539eb7c1582eae65359d
Phu Vo <phu.vo@go1.com>
2024-11-01 19:24:56 +0700

    Use `tsx` instead of `ts-node`

---
+++ src/print.ts
@@ -0,0 +1,20 @@
+import boxen from 'boxen';
+
+const languageMap: Record<string, string> = {
+	js: 'JavaScript',
+};
+
+const codeBlockRegex = /```(\w+)?\n([\s\S]*?)\n```/g;
+
+export function printMessage(text: string) {
+	const result = text.replace(codeBlockRegex, (_, language, code) => {
+		const title = languageMap[language] || language;
+		return boxen(code, {
+			borderStyle: 'round',
+			dimBorder: true,
+			padding: { top: 1, right: 2, bottom: 1, left: 2 },
+			title,
+		});
+	});
+	console.log(result);
+}
